aa50fd9011edd3d69a3ddc3d58fdced01cf315e0
Aleesha <aleeshajaleel31@gmail.com>
2023-07-03 22:05:13 +0530

    post request added

--- detection/agedetection/views.py
+++ detection/agedetection/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, './agedetection/index.html')
+    if request.method == 'POST':
+     return render(request, './agedetection/index.html')
